ba53ec5e9b1a21c5d124d5f89f05a6eec17762c0
yangzhifei <yzf140@outlook.com>
2024-04-07 07:01:21 +0800

    修改配置

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -38,7 +38,7 @@ export function InfoCard(props) {
                 <Announcement post={notice} style={{ color: 'white !important' }} />
             </div>
 
-            <div className='flex justify-between'>
+            {/*<div className='flex justify-between'>
                 <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
                     {/* 两个社交按钮 */}
                     <div className='bg-indigo-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
@@ -51,8 +51,8 @@ export function InfoCard(props) {
                         }
                     </div>
                 </div>
-               /* <MoreButton /> */
-            </div>
+               <MoreButton />
+            </div>*/}
         </Card>
   )
 }
